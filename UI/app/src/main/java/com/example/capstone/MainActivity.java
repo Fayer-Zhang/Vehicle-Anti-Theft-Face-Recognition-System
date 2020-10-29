9260556376693cfcb855a156bae5f49cdaaf2b83
joelalexpereira <57968243+joelalexpereira@users.noreply.github.com>
2020-10-29 19:42:13 -0400

    Update MainActivity.java

--- UI/app/src/main/java/com/example/capstone/MainActivity.java
+++ UI/app/src/main/java/com/example/capstone/MainActivity.java
@@ -2,13 +2,43 @@ package com.example.capstone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login;
+    private Button sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = (Button) findViewById(R.id.signin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+
+        sign = (Button) findViewById(R.id.signup);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignup();
+            }
+        });
+    }
+
+    public void openLogin() {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+    }
+
+    public void openSignup() {
+        Intent intent2 = new Intent(this, signup.class);
+        startActivity(intent2);
     }
 }
