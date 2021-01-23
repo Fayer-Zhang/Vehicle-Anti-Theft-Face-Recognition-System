34dc9a6088d8544271208ddd245800e7383ef8f8
Batuhan Basoglu <bbaso079@uottawa.ca>
2021-01-22 20:55:35 -0500

    Updated the Remove_Face template.

--- Remove_Face.py
+++ Remove_Face.py
@@ -1,3 +1,14 @@
 import DBHelper
 
-def remove_your_face(firstname, lastname, email, phone):
+def remove_your_face():
+    users = DBHelper.db.child("Users").get()
+    try:
+        count = 0
+        for user in users.each():
+            count += 1
+        DBHelper.upload_data("User_" + str(count), firstname, lastname, email, phone)
+        Facial_Recognition_Registration.register_your_face("User_" + str(count))
+        for i in range(20):
+            DBHelper.upload_user_photo("User_" + str(count) + "/" + str(i) + ".jpg")
+    except:
+        print("No Users are registered.")
